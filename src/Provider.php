ef049c9b9509af2aab879322f8045cbf1baa49c0
iiiicaro <icaro.isf@gmail.com>
2016-06-10 13:57:51 -0300

    Fixed a bug with the getAccessToken() method. Name changed.

--- src/Provider.php
+++ src/Provider.php
@@ -66,7 +66,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     /**
      * {@inheritdoc}
      */
-    public function getAccessToken($code)
+    public function getAccessTokenResponse($code)
     {
         $response = $this->getHttpClient()->post($this->getTokenUrl(), [
             'headers' => [
